9d8fa7c4e252e3e16d6eec41ebdaef81a540a3bf
DanielDevAmaral <Daniel.amaral1928@outlook.com>
2023-09-30 08:46:36 -0300

    Creating a menu

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -8,6 +8,10 @@ class Program
         Scripture myScripture = new("Nicodemus saith unto him, How can a man be born when he is old? can he enter the second time into his mother’s womb, and be born? Jesus answered, Verily, verily, I say unto thee, Except a man be born of water and of the Spirit, he cannot enter into the kingdom of God.", myReference);
         
         Console.Clear();
+        Console.WriteLine("\nChoose the difficulty");
+        Console.WriteLine("\n1. Easy mode\n2. Medium mode\n3. Hard mode\n(Please just type the number)");
+        string difficulty = Console.ReadLine();
+        int selectDifficulty = int.Parse(difficulty);
 
         while (!myScripture.IsCompletelyHidden())
         {
@@ -21,7 +25,7 @@ class Program
                 break;
             }
 
-            myScripture.HideRandomWords(5);
+            myScripture.HideRandomWords(selectDifficulty);
             Console.Clear();
         }
 
