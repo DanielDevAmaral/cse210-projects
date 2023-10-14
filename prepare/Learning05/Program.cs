e3512e7ec908346d7204c35e2856ac4aec75d738
DanielDevAmaral <Daniel.amaral1928@outlook.com>
2023-10-14 18:45:05 -0300

    learning polymorphism

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,26 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+
+
+        Rectangle rectangle = new(4.1,8.4, "blue");
+        Square square = new(4.2, "grey");
+        Circle circle = new(4,"yellow");
+        
+        List<Shape> lista = new List<Shape>();
+        lista.Add(square);
+        lista.Add(rectangle);
+        lista.Add(circle);
+
+        foreach(Shape itens in lista)
+        {
+            Console.WriteLine(itens.GetArea());
+            Console.WriteLine(itens.GetColor());
+        }
+
     }
 }
